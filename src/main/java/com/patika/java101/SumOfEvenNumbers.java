2c60196b529c03ce8ff3e206a42f07e6b5174fdd
Serhat Demirkol <sdemirkol@perfektive.com>
2022-01-24 17:04:04 +0300

    SumOfEvenNumbers added

---
+++ src/main/java/com/patika/java101/SumOfEvenNumbers.java
@@ -0,0 +1,24 @@
+package com.patika.java101;
+
+import java.util.Scanner;
+
+public class SumOfEvenNumbers {
+    public static void main(String[] args) {
+
+        int k;
+        int sum=0;
+        int count=0;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number:");
+        k = input.nextInt();
+
+        for (int i=1; i<=k; i++) {
+            if (i % 3 == 0 & i % 4 == 0) {
+                count++;
+                sum += i;
+        }
+    }
+    System.out.print("Average of the numbers from 1 to "+k+" divisible by 3 and 4  : "+sum/count);
+    }
+    
+}
