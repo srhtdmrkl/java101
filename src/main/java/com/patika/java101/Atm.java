4659cfccd2a36de03544a903ea235c666c559889
Serhat Demirkol <srhtdmrkl@gmail.com>
2022-01-25 22:20:20 +0300

    ATM added

---
+++ src/main/java/com/patika/java101/Atm.java
@@ -0,0 +1,65 @@
+package com.patika.java101;
+
+import java.util.Scanner;
+
+public class Atm {
+    public static void main(String[] args) {
+        
+        String userName, password;
+        Scanner input = new Scanner(System.in);
+        int right = 3;
+        int balance = 1500;
+        int select;
+        while (right > 0) {
+            System.out.print("Kullanıcı Adınız : ");
+            userName = input.nextLine();
+            System.out.print("Parolanız : ");
+            password = input.nextLine();
+
+            if (userName.equals("patika") && password.equals("dev123")) {
+                System.out.println("Merhaba, Kodluyoruz Bankasına Hoşgeldiniz!");
+                while(true) {
+                    System.out.println("1-Para yatırma\n" +
+                            "2-Para Çekme\n" +
+                            "3-Bakiye Sorgula\n" +
+                            "4-Çıkış Yap");
+                    System.out.print("Lütfen yapmak istediğiniz işlemi seçiniz : ");
+                    select = input.nextInt();
+                    switch(select) {
+                        case 1:
+                            System.out.print("Para miktarı : ");
+                            int deposit = input.nextInt();
+                            balance += deposit;
+                            break;
+                        case 2:
+                            System.out.print("Para miktarı : ");
+                            int withdraw = input.nextInt();
+                            if (withdraw > balance) {
+                                System.out.println("Bakiye yetersiz.");
+                            } else {
+                            balance -= withdraw;
+                            }
+                            break;
+                        case 3:
+                            System.out.println("Bakiyeniz : " + balance);
+                            break;
+                        case 4:
+                            System.out.println("Tekrar görüşmek üzere.");
+                            System.exit(0);
+                        default:
+                            System.out.println("Lütfen geçerli bir işlem seçin.");
+                    }
+                } 
+            } else {
+                right--;
+                System.out.println("Hatalı kullanıcı adı veya şifre. Tekrar deneyiniz.");
+                if (right == 0) {
+                    System.out.println("Hesabınız bloke olmuştur lütfen banka ile iletişime geçiniz.");
+                } else {
+                    System.out.println("Kalan Hakkınız : " + right);
+                }
+            }
+        }
+    }
+    
+}
